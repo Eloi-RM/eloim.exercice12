2f88cbdc32fc2fbb56f4eb52dd2cfa27f813069e
Eloi RM <eloimailmaynad@gmail.com>
2025-01-28 16:13:31 +0100

    ca marche a peu pres

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const lcdScreen = document.querySelector("#lcd-screen")
 const allPrimaryButtons = document.querySelectorAll(".btn-primary")
-const plusButton = document.querySelector("#plus-button")
 const equalButton = document.querySelector("#equal-button")
 const resetButton = document.querySelector("#reset-button")
-
-let equation
+const allSecondaryButtons = document.querySelectorAll(".btn-secondary")
 
 allPrimaryButtons.forEach(button => {
     button.addEventListener('click', () => {
         lcdScreen.textContent += button.textContent
     })
-})  
+})
 
-plusButton.addEventListener('click', ()=> {
-    lcdScreen.textContent += " " + "+" + " "
+allSecondaryButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        lcdScreen.textContent += button.value
+    })
 })
 
 resetButton.addEventListener('click', () => {
@@ -26,17 +26,60 @@ equalButton.addEventListener('click', () => {
 
 function readScreen() {
     const arr = lcdScreen.textContent.split(" ")
+    let tempResult = 0
 
     console.log(arr)
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === "+") {
-            console.log(doSumOfAB(parseFloat(arr[i-1]), parseFloat(arr[i+1])))
+        if (arr[i] === "*" || arr[i] === "/") {
+            switch (arr[i]) {
+                case "*":
+                    tempResult += doMultiplicationOfAB(arr[i-1], arr[i+1])
+                    arr[i-1] = 0
+                    arr[i+1] = 0
+                    break
+            
+                case "/":
+                    tempResult += doDivisionOfAB(arr[i-1], arr[i+1])
+                    arr[i-1] = 0
+                    arr[i+1] = 0
+                    break
+            }
         }
     }
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case "+":
+                tempResult += doAdditionOfAB(arr[i-1], arr[i+1])
+                arr[i-1] = 0
+                arr[i+1] = 0
+                break
+        
+            case "-":
+                tempResult += doSubstractionOfAB(arr[i-1], arr[i+1])
+                arr[i-1] = 0
+                arr[i+1] = 0
+                break
+        }
+    }
+
+    console.log(tempResult)
 }
 
 
-function doSumOfAB(a, b) {
-    return a + b
+function doAdditionOfAB(a, b) {
+    return parseFloat(a) + parseFloat(b)
+}
+
+function doSubstractionOfAB(a, b) {
+    return parseFloat(a) - parseFloat(b)
+}
+
+function doMultiplicationOfAB(a, b) {
+    return parseFloat(a) * parseFloat(b)
+}
+
+function doDivisionOfAB(a, b) {
+    return parseFloat(a) / parseFloat(b)
 }
